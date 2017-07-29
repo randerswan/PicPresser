a8301054387e3a57056cb9cdc1af98d89b9e417c
liangxiwen86@gmail.com <liangxiwen86@gmail.com>
2017-07-29 22:25:54 +0800

    PicPresser basic code.

---
+++ src/main/java/cn/liangxiwen/picpresser/PicPressMainActivity.java
@@ -0,0 +1,13 @@
+package cn.liangxiwen.picpresser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PicPressMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pic_press_main);
+    }
+}
